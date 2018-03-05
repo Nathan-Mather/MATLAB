2304f5fac694f2334181c36e6235b6296b423760
Nathan-Mather <njmather@umich.edu>
2018-03-05 14:52:24 -0500

    remove temp folder

--- dynare/temp/hw3_q3_dynare_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = hw3_q3_dynare_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
